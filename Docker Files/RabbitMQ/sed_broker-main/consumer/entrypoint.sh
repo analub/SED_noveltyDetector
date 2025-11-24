b46572b27678f1b768ace0f5eb7d1f0a4b9f1cbb
Ana Luisa Basilio de Castro <bc.analu04@gmail.com>
2025-11-24 11:49:16 -0300

    feat: linkagem classe arquivosTxt

--- Docker Files/RabbitMQ/sed_broker-main/consumer/entrypoint.sh
+++ Docker Files/RabbitMQ/sed_broker-main/consumer/entrypoint.sh
@@ -8,9 +8,12 @@ g++ -std=c++11 -Wall -fexceptions -g -pthread -I inc -I libs/inc -c src/consumer
 g++ -std=c++11 -Wall -fexceptions -g -pthread -I inc -I libs/inc -c src/filtro_nutall.cpp -o bin/filtro_nutall.o
 g++ -std=c++11 -Wall -fexceptions -g -pthread -I libs/inc -c libs/src/rmqbroker.cpp -o bin/rmqbroker.o
 g++ -std=c++11 -Wall -fexceptions -g -pthread -I inc -I libs/inc -c src/noveltyDetector.cpp -o bin/noveltyDetector.o
+g++ -std=c++11 -Wall -fexceptions -g -pthread -I inc -I libs/inc -c src/arquivoTxt.cpp -o bin/arquivoTxt.o
+
+
 
 # Linkagem correta
-g++ -std=c++11 -o consumer_module bin/rmqbroker.o bin/consumer.o bin/main.o bin/noveltyDetector.o -L/usr/local/lib -lz -lpthread -lSimpleAmqpClient
+g++ -std=c++11 -o consumer_module bin/rmqbroker.o bin/consumer.o bin/main.o bin/noveltyDetector.o bin/ArquivoTxt.o -L/usr/local/lib -lz -lpthread -lSimpleAmqpClient
 
 # Permissões e execução
 chmod +x ./consumer_module
